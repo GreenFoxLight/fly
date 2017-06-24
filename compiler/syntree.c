03f59042991dcbc6bb5f7ecbb380fdf42153eadf
Kevin Trogant <greenfoxlight.42@gmail.com>
2017-06-24 21:52:03 +0200

    Freeing all synentries

--- compiler/syntree.c
+++ compiler/syntree.c
@@ -18,7 +18,11 @@ int init_syntree(syntree_t* tree) {
 }
 
 void release_syntree(syntree_t* tree) {
-    /* TODO: Free entry data */
+    for (size_t i = 0; i < tree->num_entries; i++) {
+        if (tree->entries[i].type == TYPE_LIST) {
+            free(tree->entries[i].value.list.list);
+        }
+    }
     free(tree->entries);
     tree->num_entries = 0;
 }
@@ -62,6 +66,7 @@ internal ast_id add_entry(syntree_t* tree, synentry_t entry) {
 ast_id syntree_add_int(syntree_t* tree, i32 i) {
     synentry_t node;
     node.tag = AST_CONST_INT;
+    node.type = TYPE_LEAF;
     node.value.integer = i;
     return add_entry(tree, node);
 }
@@ -69,6 +74,7 @@ ast_id syntree_add_int(syntree_t* tree, i32 i) {
 ast_id syntree_add_uint(syntree_t* tree, u32 u) {
     synentry_t node;
     node.tag = AST_CONST_UINT;
+    node.type = TYPE_LEAF;
     node.value.unsigned_int = u;
     return add_entry(tree, node);
 }
@@ -76,6 +82,7 @@ ast_id syntree_add_uint(syntree_t* tree, u32 u) {
 ast_id syntree_add_long(syntree_t* tree, i64 l) {
     synentry_t node;
     node.tag = AST_CONST_INTL;
+    node.type = TYPE_LEAF;
     node.value.long_int = l;
     return add_entry(tree, node);
 }
@@ -83,6 +90,7 @@ ast_id syntree_add_long(syntree_t* tree, i64 l) {
 ast_id syntree_add_ulong(syntree_t* tree, u64 u) {
     synentry_t node;
     node.tag = AST_CONST_UINTL;
+    node.type = TYPE_LEAF;
     node.value.unsigned_long = u;
     return add_entry(tree, node);
 }
@@ -90,6 +98,7 @@ ast_id syntree_add_ulong(syntree_t* tree, u64 u) {
 ast_id syntree_add_f32(syntree_t* tree, f32 f) {
     synentry_t node;
     node.tag = AST_CONST_FLOAT32;
+    node.type = TYPE_LEAF;
     node.value.float32 = f;
     return add_entry(tree, node);
 }
@@ -97,6 +106,7 @@ ast_id syntree_add_f32(syntree_t* tree, f32 f) {
 ast_id syntree_add_f64(syntree_t* tree, f64 f) {
     synentry_t node;
     node.tag = AST_CONST_FLOAT64;
+    node.type = TYPE_LEAF;
     node.value.float64 = f;
     return add_entry(tree, node);
 }
@@ -104,6 +114,7 @@ ast_id syntree_add_f64(syntree_t* tree, f64 f) {
 ast_id syntree_add_bool(syntree_t* tree, bool b) {
     synentry_t node;
     node.tag = AST_CONST_BOOL;
+    node.type = TYPE_LEAF;
     node.value.boolean = b;
     return add_entry(tree, node);
 }
@@ -111,6 +122,7 @@ ast_id syntree_add_bool(syntree_t* tree, bool b) {
 ast_id syntree_add_char(syntree_t* tree, char c) {
     synentry_t node;
     node.tag = AST_CONST_CHAR;
+    node.type = TYPE_LEAF;
     node.value.character = c;
     return add_entry(tree, node);
 }
@@ -118,6 +130,7 @@ ast_id syntree_add_char(syntree_t* tree, char c) {
 ast_id syntree_add_string(syntree_t* tree, const char* s) {
     synentry_t node;
     node.tag = AST_CONST_STRING;
+    node.type = TYPE_LEAF;
     node.value.string = malloc(strlen(s) + 1);
     if (!node.value.string) {
         fprintf(stderr, "Out of memory!\n");
@@ -129,6 +142,7 @@ ast_id syntree_add_string(syntree_t* tree, const char* s) {
 ast_id syntree_add_id(syntree_t* tree, const char* id) {
     synentry_t node;
     node.tag = AST_ID;
+    node.type = TYPE_LEAF;
     node.value.string = malloc(strlen(id) + 1);
     if (!node.value.string) {
         fprintf(stderr, "Out of memory!\n");
@@ -141,6 +155,7 @@ ast_id syntree_add_id(syntree_t* tree, const char* id) {
 ast_id syntree_add_operator(syntree_t* tree, token_tag_t op) {
     synentry_t node;
     node.tag = AST_OPERATOR;
+    node.type = TYPE_LEAF;
     node.value.operator = op;
     return add_entry(tree, node);
 }
@@ -148,6 +163,7 @@ ast_id syntree_add_operator(syntree_t* tree, token_tag_t op) {
 ast_id syntree_add_tag(syntree_t* tree, synentry_tag_t tag, ast_id contained) {
     synentry_t node;
     node.tag = tag;
+    node.type = TYPE_TAG;
     node.value.tag = contained;
     return add_entry(tree, node);
 }
@@ -156,6 +172,7 @@ ast_id syntree_add_pair(syntree_t* tree, synentry_tag_t tag,
         ast_id first, ast_id second) {
     synentry_t node;
     node.tag = tag;
+    node.type = TYPE_TAG;
     node.value.pair.first = first;
     node.value.pair.second = second;
     return add_entry(tree, node);
@@ -166,6 +183,7 @@ ast_id syntree_add_list(syntree_t* tree, synentry_tag_t tag,
     
     synentry_t node;
     node.tag = tag;
+    node.type = TYPE_LIST;
     node.value.list.length = length;
     node.value.list.list = malloc(sizeof(ast_id) * length);
     if (!node.value.list.list) {
